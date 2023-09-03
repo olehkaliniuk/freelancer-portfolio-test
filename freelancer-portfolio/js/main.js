0acbb6fd33f0beca6082b5bb7642a3ccb22b7f35
Oleh Kaliniuk <142879479+olehkaliniuk@users.noreply.github.com>
2023-09-03 16:53:21 +0200

    Add files via upload

---
+++ freelancer-portfolio/js/main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+
+navBtn.onclick = () => {
+  if (nav.classList.toggle("open")) {
+    navBtnImg.src = "./img/NAVCLOSE.svg";
+  } else {
+    navBtnImg.src = "./img/NAV.svg";
+  }
+};
+
+AOS.init();
